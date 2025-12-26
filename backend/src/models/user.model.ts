c4c66a44c9a2c4b6413a18ec6f23fe43eb16a275
*vivek <vrvivekofficial@gmail.com>
2025-12-26 12:20:10 +0530

    feat: add User mongoose model

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,57 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { compareValue, hashValue } from "../utils/bcrypt";
+
+export interface UserDocument extends Document {
+  name: string;
+  email: string;
+  password: string;
+  profilePicture: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+  comparePassword: (password: string) => Promise<boolean>;
+  omitPassword: () => Omit<UserDocument, "password">;
+}
+
+export const userSchema = new Schema<UserDocument>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: { type: String, select: true,required:true },
+    profilePicture: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function () {
+  if (this.isModified("password") && this.password) {
+    this.password = await hashValue(this.password);
+  }
+});
+
+userSchema.methods.omitPassword = function (): Omit<UserDocument, "password"> {
+  const userObject = this.toObject();
+  delete userObject.password;
+  return userObject;
+};
+
+userSchema.methods.comparePassword = async function (value: string) {
+  return compareValue(value, this.password);
+};
+
+const UserModel = mongoose.model<UserDocument>("User", userSchema);
+export default UserModel;
