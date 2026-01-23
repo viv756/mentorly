00143901d70b0d2995d0aecce35fc087b63957ff
*vivek <vrvivekofficial@gmail.com>
2026-01-23 12:42:36 +0530

    created inbox page and sessionRequest card

---
+++ frontend/src/pages/dashboard/inbox/_components/declineSessionRequest-modal.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import React from "react";
+
+const DeclineSessionRequestModal = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant={"ghost"}>Decline</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your account and remove your
+            data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      <DialogFooter className="">
+        <DialogClose asChild>
+          <Button type="button">Close</Button>
+        </DialogClose>
+      </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DeclineSessionRequestModal;
--- frontend/src/pages/dashboard/inbox/index.tsx
+++ frontend/src/pages/dashboard/inbox/index.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import SessionRequestCard from "./_components/card";
 
 const Inbox = () => {
-  return <div>
-    inbox
-  </div>;
+
+  
+
+  return (
+    <>
+      <h1 className="text-4xl mt-5 font-semibold">Session Requests</h1>
+      <div className="grid sm:grid-cols-2  gap-2 mt-10">
+        <SessionRequestCard />
+        <SessionRequestCard />
+        <SessionRequestCard />
+      </div>
+    </>
+  );
 };
 
 export default Inbox;
