cb949c806def8e4c7ef9e6abc5bf6c10eb84aca8
*vivek <vrvivekofficial@gmail.com>
2026-01-29 12:13:37 +0530

    fix: fixed date display

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import Navbar from "./_components/navbar";
 import HeroLanding from "./_components/hero";
+import { Meteors } from "@/components/ui/meteors";
 
 const Home = () => {
   return (
     <>
       <div className="sm:px-30 px-2 ">
+        <Meteors/>
         <Navbar />
         <HeroLanding />
       </div>
