39de71f6d65ea14de600b96f2891e59474876e32
*vivek <vrvivekofficial@gmail.com>
2026-02-04 14:17:36 +0530

    fixed ui

--- frontend/src/pages/dashboard/skills/_components/userSkillsSection.tsx
+++ frontend/src/pages/dashboard/skills/_components/userSkillsSection.tsx
@@ -1,3 +1,4 @@
+import { BrainCog } from "lucide-react";
 import type { UserSkill } from "@/features/skills/types";
 import SkillCard from "./skill-card";
 import type { SKILL_ENUM_TYPE } from "@/constant";
@@ -10,9 +11,10 @@ type UserSkillsSectionProps = {
 const UserSkillsSection = ({ skills }: UserSkillsSectionProps) => {
   if (skills.length === 0) {
     return (
-      <div className="flex items-center justify-center h-full ">
-        <div className="h-full">
-          <h1>Add skills to show</h1>
+      <div className="h-full w-full flex items-center justify-center mt-30 ">
+        <div className="flex flex-col items-center justify-center">
+          <BrainCog size={40} />
+          <h1 className="text-3xl font-bold">Add skills to show</h1>
         </div>
       </div>
     );
